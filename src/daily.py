e356bc393166e0ba4e2d385f0306bbc09aca135a
Connor Murphy <connor@murphysites.com>
2024-03-14 01:52:32 -0400

    add options for text wrapping and file wrapping

--- src/daily.py
+++ src/daily.py
@@ -6,6 +6,7 @@ import re
 import subprocess
 from textwrap import wrap
 from pathlib import Path
+import textwrap
 
 # here I am using %L for entry title, and %E for entry text
 # the directives unaffected by strftime are ioqvEJKLNOQ
@@ -34,6 +35,21 @@ def open_in_editor(file):
     subprocess.call([editor, file])
     return
 
+def wrap_file(file,wrap_width):
+    with open(file, 'r') as f:
+        contents = f.read()
+        contents = wrap_preserving_newlines(contents,wrap_width)
+    with open(file, 'w') as f:
+        f.write(contents)
+    return
+
+def wrap_preserving_newlines(string_to_wrap,wrap_width):
+    wrapper = textwrap.TextWrapper(width=wrap_width)
+    lines = string_to_wrap.split('\n')
+    line_list = [wrapper.fill(i) for i in lines]
+    string_with_newlines = '\n'.join(line_list)
+    return string_with_newlines
+
 def load_conf(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
@@ -43,10 +59,16 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Make an entry in your markdown diary.")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    write_parser = subparsers.add_parser('write', help="(DEFAULT) Write an entry to todays file, create it if not exists")
-    write_parser.add_argument('-a', '--append', action='store_true', help="append to last entry without new header line")
-    write_parser.add_argument('-t', '--title', help="Write the header to be entered inline")
-    write_parser.add_argument('-e', '--entry', help="Write the text to be entered inline")
+    all_entry_parser = argparse.ArgumentParser(add_help=False)
+    all_entry_parser.add_argument('-w', '--wrap-file', action="store_true", help="Wrap the entire contents of today's entry")
+
+    write_parser = subparsers.add_parser('write', parents=[all_entry_parser], help="(DEFAULT) Write an entry to todays file, create it if not exists")
+
+    add_entry_parser = subparsers.add_parser('add', parents=[all_entry_parser], help="Add an entry inline")
+    add_entry_parser.add_argument('-m', '--mood', metavar=('+/-/=', 'mood'), required=True, nargs='+', help="The mood character and word(s) to be included")
+    add_entry_parser.add_argument('-e', '--entry',nargs=1, required=True, help="Write the text to be entered inline")
+
+    edit_parser = subparsers.add_parser('edit', parents=[all_entry_parser], help="Edit today's entry without adding any headers")
 
     list_parser = subparsers.add_parser('list', help="List existing entries")
 
